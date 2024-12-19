312cd8963e6c4b17cc9e21c04431d0d8439b3166
Johneric Pangan <johneric@fuze.us>
2024-12-19 18:00:22 +0400

    feat: async rewrites 3

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import "./globals.css";
 
@@ -30,6 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
