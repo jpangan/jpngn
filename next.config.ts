2c6f8bcede5ae72563670419fa45e25dd272edb5
Johneric Pangan <johneric@fuze.us>
2024-12-19 17:10:51 +0400

    feat: update next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
-import type { NextConfig } from "next";
+import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*', // Allows all origins, adjust as needed
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
